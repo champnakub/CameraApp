41e2ec461d6eabc07e474c595ccf83587fa47e8b
charoenvit <charoenvit@charoenvit-pc>
2015-11-30 18:05:39 +0700

    Update 11/28/2015
    @Author Charoenvit Sammanont
    
    --Update application structure

--- www/app.js
+++ www/app.js
@@ -6,14 +6,12 @@
 // Declare app level module which depends on views, and components
 var App = angular.module('myApp', [
     'ngRoute',
-    
-    'myApp.version', 
+    'myApp.version',
     //VIEW SECTION
     'myApp.ItemView',
     'myApp.LoginView',
     'myApp.CameraView',
     'myApp.WSTestView',
-    
     //ANGULAR LIBRARY SECTION
     'angular-carousel',
     'angular-websql',
@@ -21,19 +19,18 @@ var App = angular.module('myApp', [
 ]);
 
 //constant for underscore.js
-App.factory('_', ['$window', function($window) {
-  return $window._; // assumes underscore has already been loaded on the page
-}]);
+App.factory('_', ['$window', function ($window) {
+        return $window._; // assumes underscore has already been loaded on the page
+    }]);
 
 //constant for webservice
 App.constant('WebService', {
-   url : 'http://beau888.dyndns.org:222/DataService/' 
+    url: 'http://beau888.dyndns.org:222/DataService/'
 });
 
 //controller
 App.controller('MainController', ['$scope', '$webSql', function ($scope, $webSql) {
-        
-        
+
         console.log('-----------------------WELCOME TO CHAMP WORLD-----------------------');
         console.log('-----------------------APP INSTANTIATED-----------------------');
 
@@ -43,6 +40,71 @@ App.controller('MainController', ['$scope', '$webSql', function ($scope, $webSql
         //@param3 = Text Description
         //@param4 = Size of Database
         $scope.db = $webSql.openDatabase('CameraApp', '1.0', 'CameraApp DB', 2 * 1024 * 1024);
+
+        //create table [* CUSTOMER]
+        //@ method [GetCustomerData()]
+        $scope.db.createTable('CUSTOMER', {
+            "ID": {
+                "type": "INTEGER",
+                "null": "NOT NULL", // default is "NULL" (if not defined)
+                "primary": true, // primary
+                "auto_increment": true // auto increment
+            },
+            "Code": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            },
+            "Description": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            }
+        });
+
+        //create table [* Project]
+        //@ method [getProjectData()]
+        $scope.db.createTable('Project', {
+            "ID": {
+                "type": "INTEGER",
+                "null": "NOT NULL", // default is "NULL" (if not defined)
+                "primary": true, // primary
+                "auto_increment": true // auto increment
+            },
+            "Customer": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            },
+            "Code": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            },
+            "Description": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            }
+        });
+
+        //create table [* Building]
+        //@ method [getBuildingData()]
+        $scope.db.createTable('Building', {
+            "ID": {
+                "type": "INTEGER",
+                "null": "NOT NULL", // default is "NULL" (if not defined)
+                "primary": true, // primary
+                "auto_increment": true // auto increment
+            },
+            "Code": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            },
+            "Project": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            },
+            "Description": {
+                "type": "TEXT",
+                "null": "NOT NULL"
+            }
+        });
     }]);
 
 //routing modules
@@ -52,22 +114,18 @@ App.config(['$routeProvider', function ($routeProvider) {
                     templateUrl: 'camera_components/camera.html',
                     controller: 'CameraCtrl'
                 }).
-                        
                 when('/loginView', {
                     templateUrl: 'login_components/login.html',
                     controller: 'LoginCtrl'
                 }).
-                        
                 when('/itemView', {
                     templateUrl: 'item_components/item.html',
                     controller: 'ItemCtrl'
-                }).       
-                        
+                }).
                 when('/wsTestView', {
                     templateUrl: 'webservice_components/ws.html',
                     controller: 'WSCtrl'
-                }).                
-                        
+                }).
                 otherwise({redirectTo: '/wsTestView'});
     }]);
 
